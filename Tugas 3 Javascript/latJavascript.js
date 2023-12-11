e4b9fb0abba974ec6d793a9bb977e0f795242969
HRF-10 <140594929+HRF-10@users.noreply.github.com>
2023-12-11 09:50:20 +0700

    Tugas Pemrograman Web kelas 11 Semester 1
    - Tugas pertemuan 1: membuat kode html yang memuat sebuah halaman web
    - Tugas 1: membuat website blog pribadi
    - Tugas 2: membuat website bebas dengan gabungan html dan css
    - Tugas 3: membuat allert dengan Javascript
    - Tugas 4: membuat projek dari materi 4 yang telah diberikan yaitu php, database, dan relasi antar tabel di sebuah database

---
+++ Tugas 3 Javascript/latJavascript.js
@@ -0,0 +1,9 @@
+var namaSiswa = 'Hanif Raditya Firmansyah'
+const usiaSiswa = 16
+let tinggiBadan = 170
+let beratBadan = 43
+let status = null
+
+console.log('nama anda adalah ' + namaSiswa + 'usia anda saat ini adalah ' + usiaSiswa + 'tinggi badan saya ' + tinggiBadan + 'berat badan saya ' + beratBadan + 'status '+status)
+
+alert('nama anda adalah ' + namaSiswa + 'usia anda saat ini adalah ' + usiaSiswa + 'tinggi badan saya ' + tinggiBadan + 'berat badan saya ' + beratBadan + 'status '+status)
